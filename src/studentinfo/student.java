6e8daff248fa0c31b746c619b9134f98154a6a8c
molham Altraboulsi <105880797+Molham007@users.noreply.github.com>
2022-06-01 00:04:19 -0400

    Update student.java

--- src/studentinfo/student.java
+++ src/studentinfo/student.java
@@ -7,7 +7,7 @@ package studentinfo;
  */
 public class student {
            private String name;
-           string address;
+           String address;
 
     public student(String name) {
         this.name = name;
